63e17c3d411c3271610bbd8b98fbff22dc9a195a
Guandaru254 <leroykim82@gmail.com>
2024-02-28 15:16:09 +0300

    Add files via upload

---
+++ user_input.py
@@ -0,0 +1,7 @@
+# ask for user input
+name = input("What is your name?")
+age = input("What is your age?")
+location = input("What is your location?")
+
+# print personalized message
+print(f"Hello {name}, you are {age} years old and live in {location}.")
